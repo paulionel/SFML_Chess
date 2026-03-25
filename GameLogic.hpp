e053528b3e94136c6703ec108394153f44252a9f
paulionel <paul.steiner98@gmail.com>
2026-03-24 20:43:04 -0500

    Add intial function definition for move logic

--- GameLogic.hpp
+++ GameLogic.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <vector>
+#include <utility>
 
 // Piece and Color definitions
 constexpr uint8_t EMPTY = 0;      // 0000 0000
@@ -12,3 +14,5 @@ constexpr uint8_t KING = 6;       // 0000 0110
 constexpr uint8_t PIECE_MASK = 7; // 0000 0111
 constexpr uint8_t BLACK = 8;      // 0000 1000
 constexpr uint8_t WHITE = 0;      // 0000 0000
+
+std::vector<std::pair<int, int>> getPossibleRookMoves();
